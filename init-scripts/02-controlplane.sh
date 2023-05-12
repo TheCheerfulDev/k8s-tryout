5aa1b5d23cc6f857fe576ea510c5dbca13070dc6
thecheerfuldev <mark@markhendriks.nl>
2023-05-12 21:45:14 +0200

    Initial commit

--- init-scripts/02-controlplane.sh
+++ init-scripts/02-controlplane.sh
@@ -13,12 +13,12 @@ sleep 10
 # Install the Flannel pod network add-on
 kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml
 
-sudo bash -c "cat > /run/flannel/subnet.env" <<EOL
-FLANNEL_NETWORK=10.244.0.0/16
-FLANNEL_SUBNET=10.244.0.1/24
-FLANNEL_MTU=1450
-FLANNEL_IPMASQ=true
-EOL
+#sudo bash -c "cat > /run/flannel/subnet.env" <<EOL
+#FLANNEL_NETWORK=10.244.0.0/16
+#FLANNEL_SUBNET=10.244.0.1/24
+#FLANNEL_MTU=1450
+#FLANNEL_IPMASQ=true
+#EOL
 
 # Print the kubeadm join command to join worker nodes to the cluster
 echo "To add worker nodes to the cluster, run the following command on each worker node:"
