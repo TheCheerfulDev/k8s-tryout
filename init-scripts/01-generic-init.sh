dd6d0b4efee2c5f4b23dd2b5ce1169b74a3bc753
thecheerfuldev <mark@markhendriks.nl>
2023-06-01 09:47:04 +0200

    updated init script

--- init-scripts/01-generic-init.sh
+++ init-scripts/01-generic-init.sh
@@ -2,12 +2,14 @@
 
 # Update the apt package index
 sudo apt update
+sudo apt upgrade -y
 
 # Install containerd and other dependencies
 sudo apt install -y containerd apt-transport-https ca-certificates curl
 
 # Disable swap
 sudo sed -i "s/\/swap.img/#\/swap.img/" /etc/fstab
+sudo swapoff -a
 
 # Prepare containerd config
 sudo mkdir /etc/containerd
@@ -21,19 +23,11 @@ sudo sed -i "s/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/" /etc/sysctl.conf
 echo "br_netfilter" | sudo tee /etc/modules-load.d/k8s.conf &>/dev/null
 
 # Install Kubernetes tools
-sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt update
 sudo apt install -y kubeadm kubelet kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
-
-# Set new hostname
-read -p "Please type the desired hostname: " NEW_HOSTNAME
-sudo hostnamectl set-hostname "$NEW_HOSTNAME"
-
-# Set new IP
-read -p "Please type the desired ip (only the last number in the 3X range): " NEW_IP
-sudo sed -i "s/192.168.178.30/192.168.178.$NEW_IP/" /etc/netplan/00-installer-config.yaml
 
 # Reboot
 echo "The system will reboot in 5 seconds..."
