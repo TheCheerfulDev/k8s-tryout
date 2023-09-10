c2ea2bc6413d0df33614a3d384e6cb9c9b860813
thecheerfuldev <mark@markhendriks.nl>
2023-09-10 12:46:24 +0200

    kubeadm init fix

--- init-scripts/02-controlplane.sh
+++ init-scripts/02-controlplane.sh
@@ -1,5 +1,5 @@
 # Initialize the Kubernetes cluster
-sudo kubeadm init --config=$HOME/kubeadm-config.yaml --node-name $(hostname)
+sudo kubeadm init --control-plane-endpoint=$(hostname -I) --node-name $(hostname) --pod-network-cidr=10.244.0.0/16
 
 # Copy the kubeconfig file to the current user's home directory
 mkdir -p $HOME/.kube
